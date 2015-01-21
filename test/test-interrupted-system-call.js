8e99365d9d8b5792aa534a35de0d0506fd12dae8
Elijah Insua <tmpvar@gmail.com>
2015-01-21 12:23:33 -0700

    make broker testable and add failing EINTR test

---
+++ test/test-interrupted-system-call.js
@@ -0,0 +1,83 @@
+var test = require('tape');
+var omdp = require('../');
+var MDP = require('../lib/mdp')
+
+
+test('catch interrupted system call', function(t) {
+
+  var location = 'inproc://#1';
+  var broker = new omdp.Broker(location, {
+    HEARTBEAT_INTERVAL : 1
+  });
+  var worker = new omdp.Worker(location, 'echo');
+  worker.start();
+
+  // hotwire the addition of this worker to make the test run
+  // immediately.
+  broker.workers[worker.name] = worker;
+
+  // wrap heartbeat in a try/catch to detect errors
+  var heartbeat = broker.heartbeat;
+  broker.heartbeat = function override(workerId) {
+    try {
+      heartbeat.call(broker, workerId);
+    } catch (e) {
+      t.fail();
+    }
+    broker.stop();
+    worker.stop();
+    t.end();
+  }
+
+  broker.start(function() {
+
+    var send = broker.socket.send;
+    broker.socket.send = function sender(arr) {
+
+      if (arr[2] === MDP.W_HEARTBEAT) {
+        throw new Error('Interrupted System Call');
+      } else {
+        send.call(broker.socket, arr);
+      }
+    }
+  });
+});
+
+test('allow other errors to flow through', function(t) {
+  var location = 'inproc://#1';
+  var broker = new omdp.Broker(location, {
+    HEARTBEAT_INTERVAL : 1
+  });
+  var worker = new omdp.Worker(location, 'echo');
+  worker.start();
+
+  // hotwire the addition of this worker to make the test run
+  // immediately.
+  broker.workers[worker.name] = worker;
+
+  // wrap heartbeat in a try/catch to detect errors
+  var heartbeat = broker.heartbeat;
+  broker.heartbeat = function override(workerId) {
+    try {
+      heartbeat.call(broker, workerId);
+    } catch (e) {
+      t.fail();
+    }
+    broker.stop();
+    worker.stop();
+    t.end();
+  }
+
+  broker.start(function() {
+
+    var send = broker.socket.send;
+    broker.socket.send = function sender(arr) {
+
+      if (arr[2] === MDP.W_HEARTBEAT) {
+        throw new Error('some other thing');
+      } else {
+        send.call(broker.socket, arr);
+      }
+    }
+  });
+});
